7f6f6a520376fb14ef9a10dd75120f34f5bf2102
Adam Leinss <35347500+adamleinss@users.noreply.github.com>
2021-07-15 12:53:12 -0500

    Add files via upload

---
+++ purge_google_drive.ps1
@@ -0,0 +1,14 @@
+﻿$users = get-content D:\scripts\users.csv
+
+foreach ($user in $users) {
+
+Write-Output $user
+
+gam user $user@acme.com print filelist id > D:\scripts\files.csv
+$MyFile = Get-Content D:\scripts\files.csv
+$MyFile | Out-File -Encoding "UTF8" D:\scripts\files.csv
+gam csv D:\scripts\files.csv gam user "~Owner" delete drivefile "~id" purge
+
+}
+
+ 
